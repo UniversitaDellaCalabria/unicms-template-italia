a393805a31c9219e90c0ee25b694c730dc3b22fd
Francesco Filicetti <francesco.filicetti@unical.it>
2022-04-07 10:46:20 +0200

    feat: editorial board templatetags

--- src/unicms_template_italia/templatetags/unicms_template_italia.py
+++ src/unicms_template_italia/templatetags/unicms_template_italia.py
@@ -1,14 +1,15 @@
 import logging
 
 from django import template
-
+from django.conf import settings
 from cms.menus.models import NavigationBarItem
+
 
 logger = logging.getLogger(__name__)
 register = template.Library()
 
 
-@register.simple_tag()
+@register.simple_tag
 def is_mega_menu(item):
     if not item: return False
     childs = item.get_childs()
@@ -20,3 +21,18 @@ def is_mega_menu(item):
         if child.has_childs(): with_childs += 1
         if index>0 and with_childs: return True
     return False
+
+
+@register.simple_tag
+def editorial_board_news_add(webpath):
+    if not hasattr(settings, 'EDITORIAL_BOARD_NEWS_ADD_URL'): return '#'
+    return settings.EDITORIAL_BOARD_NEWS_ADD_URL.format(website=webpath.site.pk,
+                                                        webpath=webpath.pk)
+
+
+@register.simple_tag
+def editorial_board_news_edit(item):
+    if not hasattr(settings, 'EDITORIAL_BOARD_NEWS_EDIT_URL'): return '#'
+    return settings.EDITORIAL_BOARD_NEWS_EDIT_URL.format(website=item.webpath.site.pk,
+                                                         webpath=item.webpath.pk,
+                                                         news=item.pk)

cec022d0fe792d0e13aa1972a0153adee6d07be1
Francesco Filicetti <francesco.filicetti@unical.it>
2024-01-26 13:26:05 +0100

    feat: vuejs 2.7.16 + axios 1.6.7

--- src/unicms_template_italia/settings.py
+++ src/unicms_template_italia/settings.py
@@ -134,4 +134,4 @@ PINTEREST_ENDPOINT = 'https://www.pinterest.com/pin/create/button/?url='
 
 # CDN FOR STATICS
 UNICMS_TEMPLATE_ITALIA_USE_CDN = False
-UNICMS_TEMPLATE_ITALIA_CDN = 'https://cdn.jsdelivr.net/gh/UniversitaDellaCalabria/unicms-template-italia@1.2.1/src/unicms_template_italia/static'
+UNICMS_TEMPLATE_ITALIA_CDN = 'https://cdn.jsdelivr.net/gh/UniversitaDellaCalabria/unicms-template-italia@1.3.0/src/unicms_template_italia/static'

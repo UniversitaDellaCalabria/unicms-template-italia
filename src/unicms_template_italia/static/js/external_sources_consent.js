1aef972fffc69f5154e5e61a2b2e1ef4e60832cb
Francesco Filicetti <easyweb.wa@gmail.com>
2025-10-16 18:02:34 +0200

    fix: cookies management

--- src/unicms_template_italia/static/js/external_sources_consent.js
+++ src/unicms_template_italia/static/js/external_sources_consent.js
@@ -1,38 +1,8 @@
-const cookie_data = document.getElementById('unicms-cookie-data');
-const external_sources_consent_key = cookie_data.dataset.external_sources_consent_key;
-const external_sources_consent_key_expiration = cookie_data.dataset.external_sources_consent_key_expiration;
-const cookie_domain = cookie_data.dataset.cookie_domain;
+const cookie_data = JSON.parse(document.getElementById('unicms-cookie-data').textContent);
+const external_sources_consent_key = cookie_data.external_sources_consent_key;
+const external_sources_consent_key_expiration = cookie_data.external_sources_consent_key_expiration;
+const cookie_domain = cookie_data.cookie_domain;
 
-
-function getConsents() {
-    const consent = getCookie();
-    try {
-        const json_string = consent ? decodeURIComponent(consent) : '{}';
-        return JSON.parse(json_string);
-    } catch {
-        return {};
-    }
-}
-
-function editConsent(host, revoke=false) {
-    if (host) {
-        const data = getConsents();
-        if (!revoke) data[host] = Date.now() + external_sources_consent_key_expiration * 24 * 60 * 60 * 1000;
-        else data[host] = Date.now() - 1;
-        const consentStr = encodeURIComponent(JSON.stringify(data));
-        const expiration_value = external_sources_consent_key_expiration * 24 * 60 * 60;
-        const secure_value = window.location.protocol === "https:" ? "Secure;" : "";
-        document.cookie = `${external_sources_consent_key}=${consentStr};domain=.unical.it;${secure_value}path=/;SameSite=Lax;max-age=${expiration_value}`;
-        getCookie();
-    }
-}
-
-function hasConsent(host) {
-    if (!host) return false;
-    if (window.location.host.split(":")[0].endsWith(host)) return true;
-    const data = getConsents();
-    return data[host] !== undefined && data[host] > Date.now();
-}
 
 function getCookie() {
     const value = `; ${document.cookie}`;
@@ -45,10 +15,47 @@ function getCookie() {
 }
 
 function normalizeHost(urlString) {
+    return tldts.parse(urlString).domain;
+}
+
+function getConsents() {
+    const consent = getCookie();
     try {
-        const host = new URL(urlString).hostname.replace(/^www\./, "");
-        return host;
+        const json_string = consent ? decodeURIComponent(consent) : '{}';
+        const data = JSON.parse(json_string);
+        const to_delete = [];
+        for (const host in data) {
+            if (!normalizeHost(host)) to_delete.push(key);
+        }
+        for (const del in to_delete) {
+            delete data[del];
+        }
+        return data;
     } catch {
-        return null;
+        return {};
     }
 }
+
+function editConsent(host, revoke=false) {
+    if (!host) return;
+    const norm_host = normalizeHost(host)
+    const main_domain = normalizeHost(window.location.host)
+    const data = getConsents();
+    if (!revoke) data[norm_host] = Date.now() + external_sources_consent_key_expiration * 24 * 60 * 60 * 1000;
+    else data[norm_host] = Date.now() - 1;
+    const consentStr = encodeURIComponent(JSON.stringify(data));
+    const expiration_value = external_sources_consent_key_expiration * 24 * 60 * 60;
+    const secure_value = window.location.protocol === "https:" ? "Secure;" : "";
+    document.cookie = `${external_sources_consent_key}=${consentStr};domain=.${main_domain};${secure_value}path=/;SameSite=Lax;max-age=${expiration_value}`;
+    getCookie();
+}
+
+function hasConsent(url) {
+    if (!url) return false;
+    const host = normalizeHost(url);
+    if (!host) return false;
+    if (window.location.host.split(":")[0].endsWith(host)) return true;
+    const data = getConsents();
+    return data[host] !== undefined && data[host] > Date.now();
+}
+
